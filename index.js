fb80f3112d8e0775f5cf2d83ba20c9dfdcb59f9b
ayat <abuhajayat@gmail.com>
2024-12-12 23:31:54 +0100

    lab complete

--- index.js
+++ index.js
@@ -10,15 +10,65 @@ class SortedList {
     this.length = this.items.length;  
   }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new OutOfBounds();
+    }
+    return this.items[pos];
+  }
 
-  max() {}
 
-  min() {}
+  
+  max() {
+    if(this.items.length == 0){
+      throw new EmptySortedList();
+    }
+    return Math.max(...this.items);
+  }
+/*()
+      7) should return an EmptySortedList exception if there is no elements in the list
+      8) should return the max (highest) value in the list
+    #min()*/
 
-  sum() {}
+  
+  min() {  if(this.items.length == 0){
+    throw new EmptySortedList();
+  }
+  return Math.min(...this.items);
+}
+  /*(should return an EmptySortedList exception if there are no elements in the list
+  10) should return the min (lowest) value in the list
+  */
 
-  avg() {}
+  sum() {let sum = 0; 
+    if (this.items.length == 0 )  
+      return sum ; 
+    else  {
+      sum = this.items.reduce((acc, current) => acc + current, 0);
+      //for (let i = 0; i < this.items.length; i++) { sum += this.items[i]; } 
+    }
+
+    return sum;}
+
+
+  avg() {
+    if(this.items.length == 0){
+      throw new EmptySortedList();
+    }
+    return this.sum()/this.length;
+  }
 }
 
+module.exports = SortedList;
 
+
+/* add(item) {
+    try {
+          this.items.push(item);
+    this.items.sort((a,b) => a - b) // a - b: Ascending order / b - a: Descending order.
+    this.length = this.items.length; ;
+    } catch (error) {
+      console.error(error);
+      // Expected output: ReferenceError: nonExistentFunction is not defined
+      // (Note: the exact output may be browser-dependent)
+    } */
